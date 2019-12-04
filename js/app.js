d23aa436e4b2ae9aae24714b54ae065c7b8f4e64
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-12-04 13:51:28 -0800

    got listener working

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 const optionList = []
 
-function ThingWithHorns (img_url, title, description, keyword, horns) {
+function ThingWithHorns(img_url, title, description, keyword, horns) {
   this.img = img_url;
   this.title = title;
   this.description = description;
@@ -17,24 +17,47 @@ ThingWithHorns.prototype.renderToPage = function () {
   clone.find('h3').text(this.title);
   clone.find(':nth-child(3)').text(`Horns: ${this.horns}`);
   clone.find(':nth-child(4)').text(this.description);
-  clone.attr('id', `${this.title}`);
+  clone.attr('id', `${this.keyword}`);
   $('#photo-container').append(clone);
 }
-ThingWithHorns.prototype.createOptions = function (keyword) {
+
+ThingWithHorns.prototype.createOptions = function () {
   let optionContainer = $('select');
-  if (!optionList.includes(keyword)) {
+  if (!optionList.includes(this.keyword)) {
     optionList.push(this.keyword)
     optionContainer.append(`<option val=${this.keyword}>${this.keyword}</option>`);
   }
 }
 
-
 $.get('data/page-1.json', 'json').then(
   (data) => {
-    console.log(data);
-    data.forEach( hornedObj => {
+    data.forEach(hornedObj => {
       let hornedThings = new ThingWithHorns(hornedObj.image_url, hornedObj.title, hornedObj.description, hornedObj.keyword, hornedObj.horns);
       hornedThings.renderToPage();
-      hornedThings.createOptions(hornedThings.keyword);
+      hornedThings.createOptions();
     });
   });
+
+$('select').on('change', function () {
+  $('div').hide();
+
+  const optionText = $('select option:selected').val();
+
+
+  if ($('div').attr('id') === optionText) {
+    console.log($('div').attr('id'));
+    
+  }
+
+  $('select > option').each(function() {
+    //alert(this.text + ' ' + this.value);
+
+    if (optionText === this.text) {
+      $(`#${this.text}`).show();
+      console.log($(`#${this.text}`));
+    }
+});
+
+
+});
+
